ae9d251c29ba73e9f598d634a063d7f6ea78e80c
Radni Amonkar <amonkarradni19@gmail.com>
2025-10-20 19:54:33 +0530

    Delete repositories directory

--- repositories/catRepository.js
+++
@@ -1,5 +0,0 @@
-export const fetchCats = async (amount = 10) => {
-  const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${amount}`);
-  const data = await response.json();
-  return data.map(item => item.url); // array of cat image URLs
-};
