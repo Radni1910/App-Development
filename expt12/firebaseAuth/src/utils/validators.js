8ae788a3dae5af00981769b6bf148762fdd97030
Radni <amonkarradni19@gmail.com>
2025-10-30 20:40:16 +0530

    Revert "Add Experiment12 folder"
    This reverts commit 481c2950422273cdeb7e2173246674b3c9f29d11.

--- expt12/firebaseAuth/src/utils/validators.js
+++
@@ -1,3 +0,0 @@
-// src/utils/validators.js
-export const isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
-export const isValidPassword = (password) => typeof password === "string" && password.length >= 8;
